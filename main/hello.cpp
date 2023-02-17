086f470011b986663fbc967322da80b3e85578b6
Tulasi L <79722173+tulasi3231@users.noreply.github.com>
2023-02-17 10:15:55 +0530

    Create hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello , PES1UG20CS607\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
