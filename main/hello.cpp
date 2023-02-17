d7f8ec58d9064ff9794e66b35b055893eac1a243
Tulasi L <79722173+tulasi3231@users.noreply.github.com>
2023-02-17 21:11:19 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello , PES1UG20CS607\n";
+  cout << "Hello , from PES1UG20CS607\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
